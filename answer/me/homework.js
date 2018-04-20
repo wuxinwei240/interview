128ec26f6501dedba741f0d46761c220d5ac932f
wuxinwei <670165948@qq.com>
2018-04-21 00:48:26 +0800

    优化

--- answer/me/homework.js
+++ answer/me/homework.js
@@ -12,6 +12,33 @@ const someFunction = (type, data) => {
     let returnArr = null;
     const dataLength = data.length;
     /**
+     * 循环公用if 
+     * @param {String} ele 当前值
+     * @param {Object} hashObj 哈希对象
+     * @param {Number} y y轴值
+     * @param {Number} x x轴值
+     */
+    const loopIf = (ele, hashObj, y, x) => {
+        if(ele === type){
+            hashObj[type].push(ele);
+        }
+        if(ele === 'e'){
+            hashObj.e.push([y, x]);
+        }
+    }
+    /**
+     * 共用判断
+     * @param {object} hashObj  哈希对象
+     * @param {Number} length  长度
+     * @param {Array} arr 数组
+     */
+    const commonIf = (hashObj, length, arr) => {
+        if(hashObj[type].length === length && hashObj.e.length === 1 && !eObj[hashObj.e[0].join('')]){
+            arr.push(hashObj.e[0]); 
+            eObj[hashObj.e[0].join('')] = 'e';
+        }
+    }
+    /**
      * 行
      * @param {Number} j
      */
@@ -21,17 +48,9 @@ const someFunction = (type, data) => {
             'e' : [],
         };
         data[j].forEach((ele, m) => {
-            if(ele === type){
-                hashObj[type].push(ele);
-            }
-            if(ele === 'e'){
-                hashObj.e.push([j, m]);
-            }
+            loopIf(ele, hashObj, j, m);
         })
-        if(hashObj[type].length === data[j].length - 1 && hashObj.e.length === 1 && !eObj[hashObj.e[0].join('')]){
-            rowArr.push(hashObj.e[0]); 
-            eObj[hashObj.e[0].join('')] = 'e';
-        }
+        commonIf(hashObj, data[j].length - 1, rowArr);
         hashObj = null;
     }
     /**
@@ -46,19 +65,11 @@ const someFunction = (type, data) => {
         data.forEach((ele, m) => {
             data[m].forEach((childEle, n) => {
                 if(n === i){
-                    if(childEle === type){
-                        hashObj[type].push(ele);
-                    }
-                    if(childEle === 'e'){
-                        hashObj.e.push([m, n]);
-                    }
+                    loopIf(childEle, hashObj, m, n);
                 }
             })
         })
-        if(hashObj[type].length === dataLength - 1 && hashObj.e.length === 1 && !eObj[hashObj.e[0].join('')]){
-            colArr.push(hashObj.e[0]);  
-            eObj[hashObj.e[0].join('')] = 'e';
-        }
+        commonIf(hashObj, dataLength - 1, colArr);
         hashObj = null;
     }
     /**
@@ -79,33 +90,17 @@ const someFunction = (type, data) => {
         // 1.西北到东南方向
         for(let m = 0 ; m < dataLength; m++) {
             let item = data[m][m];
-            if(item === type){
-                hashObj1[`${type}`].push(item);
-            }   
-            if(item === 'e'){
-                hashObj1.e.push([m, m]);
-            }
+            loopIf(item, hashObj1, m, m);
             item = null;
         }
         //2.东北到西南方向
         for(let n = 0; n < dataLength; n++) {
             let item = data[n][dataLength-n-1]
-            if(item === type){
-                hashObj2[`${type}`].push(item);
-            }
-            if(item === 'e'){
-                hashObj2.e.push([n, dataLength-n-1]);
-            }
+            loopIf(item, hashObj2, n, dataLength-n-1);
             item = null;
         }
-        if(hashObj1[type].length === dataLength - 1 && hashObj1.e.length === 1 && !eObj[hashObj1.e[0].join('')]){
-            diagonalArr.push(hashObj1.e[0]);  
-            eObj[hashObj1.e[0].join('')] = 'e';
-        }
-        if(hashObj2[type].length === dataLength - 1 && hashObj2.e.length === 1 && !eObj[hashObj2.e[0].join('')]){
-            diagonalArr.push(hashObj2.e[0]);  
-            eObj[hashObj2.e[0].join('')] = 'e';
-        }
+        commonIf(hashObj1, dataLength - 1, diagonalArr);
+        commonIf(hashObj2, dataLength - 1, diagonalArr);
         hashObj1 = null;
         hashObj2 = null;
     }
@@ -124,12 +119,30 @@ const someFunction = (type, data) => {
     diagonalArr = null;
     eObj = null;
     return returnArr;
-}
+};
 
 const arr1 = [
 ['o', 'e', 'e'],
-['e', 'x', 'x'],
-['x', 'x', 'e']];
-
-const result = someFunction('x', arr1);
-console.log(result)
+['e', 'x', 'o'],
+['x', 'x', 'e']
+];
+const arr2 = [
+    ['x', 'o', 'o'],
+    ['x', 'x', 'e'],
+    ['e', 'o', 'e']
+];
+const arr3 = [
+    ['x', 'x', 'o'],
+    ['e', 'e', 'e'],
+    ['e', 'e', 'e']
+];
+const arr4 = [
+    ['o', 'o', 'o'],
+    ['e', 'e', 'e'],
+    ['e', 'e', 'e']
+];
+const result1 = someFunction('x', arr1);
+const result2 = someFunction('x', arr2);
+const result3 = someFunction('x', arr3);
+const result4 = someFunction('o', arr4);
+console.log(result1, result2, result3, result4)
